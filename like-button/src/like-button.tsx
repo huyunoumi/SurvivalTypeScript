c16156709bbfb1fc68754316e9e9ae5396c16fb6
Masato Uchida <truevr.lisbeth@gmail.com>
2026-01-06 21:13:15 +0900

    completed.

---
+++ like-button/src/like-button.tsx
@@ -0,0 +1,14 @@
+import "./like-button.css";
+import { useState } from "react"; // この行を追加
+
+export function LikeButton() {
+  const [count, setCount] = useState(999); // このように書き換える
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <button className="like" onClick={handleClick} type="button">
+      {count}
+    </button>
+  );
+}
